003bf5663b8c458cdc109967110520a17dabcbdb
Dennis Burger <dennis@dutchwebworks.nl>
2017-12-31 12:55:18 +0100

    Added basic 404 error handling in Vue router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
 import Admin from './components/Admin.vue'
+import error404 from './components/error404.vue'
 import App from './App.vue'
 
 Vue.use(VueRouter);
@@ -9,6 +10,7 @@ Vue.use(VueRouter);
 const routes = [
 	{ path: '/', component: Home },
 	{ path: '/admin', component: Admin },
+	{ path: '*', component: error404 }
 ];
 
 const router = new VueRouter({
