a08e6653cec22c492bd72a7471453c31fd576692
Dennis Burger <dennis.burger@poort80.nl>
2018-01-04 09:13:52 +0100

    Updated copy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 	<main>
-		<h1>Dutchwebworks homepage</h1>
+		<h1>Homepage</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ab, adipisci mollitia deserunt nesciunt ullam non illo reiciendis perferendis fuga.</p>
 		<p>Nisi suscipit, rem! Repellendus voluptate perferendis at animi, reprehenderit quidem illum, error veniam est perspiciatis ipsam autem harum tempore vero.</p>
 
