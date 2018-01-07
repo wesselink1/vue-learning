4e4c75556c180641b2c06dca673e097349f2c14c
Dennis Burger <dennis@dutchwebworks.nl>
2018-01-07 15:43:56 +0100

    Added basic aria-role enabled Vue dynamic components using tabs.

---
+++ src/components/DynamicComponents/About.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="about-tab" aria-labelledby="about" class="tab" tabindex="0" role="tabpanel">
+		<h3 class="about__title">About</h3>
+
+		<p class="about__body">
+			Distinctio quae nobis molestiae, ullam animi magni ratione voluptatem harum laborum. Dolore.
+		</p>
+	</div>
+</template>
+
+<style>
+	.about__title,
+	.about__body {
+		font-family: "Roboto", sans-serif;
+	}
+
+	.about__title {
+		font-weight: 300;
+		font-size: 32px;
+		color: deepskyblue;
+	}
+
+	.about__body {
+		line-height: 1.6;
+	}
+</style>
---
+++ src/components/DynamicComponents/Contact.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="contact-tab" aria-labelledby="contact" class="tab" tabindex="0" role="tabpanel">
+		<h3 class="contact__title">Contact</h3>	
+
+		<p class="contact__body">
+			Consectetur adipisicing elit. Distinctio quae nobis molestiae, ullam animi magni ratione voluptatem harum laborum. Dolore.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio quae nobis molestiae, ullam animi magni ratione voluptatem harum laborum. Dolore.
+		</p>	
+
+		<p class="contact__body">
+			Fistinctio quae nobis molestiae, ullam animi magni ratione voluptatem harum laborum. Dolore.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio quae nobis molestiae, ullam animi magni ratione voluptatem harum laborum. Dolore.
+		</p>	
+	</div>
+</template>
+
+<style>
+	.contact__title,
+	.contact__body {
+		font-family: "Roboto", sans-serif;
+	}
+
+	.contact__title {
+		font-weight: 300;
+		font-size: 32px;
+		color: deepskyblue;
+	}
+
+	.contact__body {
+		line-height: 1.6;
+	}
+</style>
