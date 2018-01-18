704ee8ffe09993bbd5b89b74db9bff89ce778d5a
Dennis Burger <dennis.burger@poort80.nl>
2018-01-18 09:44:53 +0100

    Added demo for Pug files.

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -8,6 +8,7 @@
 				<router-link tag="li" :to="{ name: 'linkCards' }" class="header__nav-item" active-class="is-active" exact-active-class="is-exact"><a>Cards</a></router-link>
 				<router-link tag="li" :to="{ name: 'linkCounter' }" class="header__nav-item" active-class="is-active" exact-active-class="is-exact"><a>Counter</a></router-link>
 				<router-link tag="li" :to="{ name: 'linkDynamicComponents' }" class="header__nav-item" active-class="is-active" exact-active-class="is-exact"><a>Dynamic Components</a></router-link>
+				<router-link tag="li" :to="{ name: 'linkPug' }" class="header__nav-item" active-class="is-active" exact-active-class="is-exact"><a>Pug</a></router-link>
 			</ul>
 		</nav>
 	</header>
@@ -46,7 +47,7 @@
 	}
 
 	.header__nav-item + .header__nav-item {
-		margin-left: 10px;		
+		margin-left: 10px;
 	}
 
 	.header__nav-item a {
