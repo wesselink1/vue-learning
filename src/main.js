1710efae459a9da9ed2b1b04bb0a94c147421432
Dennis Burger <dennis@dutchwebworks.nl>
2018-01-28 12:35:17 +0100

    Removed obsolete global custom directive.

--- src/main.js
+++ src/main.js
@@ -18,24 +18,6 @@ Vue.filter("euroCurrency", function(price){
 	});
 });
 
-Vue.directive("highlight", {
-	bind(el, binding) {
-		var delay = 0;
-		
-		if(binding.modifiers['delayed']) {
-			delay = 3000;
-		}
-
-		setTimeout(() => {
-			if (binding.arg == 'background') {
-				el.style.backgroundColor = binding.value;
-			} else {
-				el.style.color = binding.value;
-			}
-		}, delay);		
-	}
-});
-
 new Vue({
 	el: "#app",
 	router,
