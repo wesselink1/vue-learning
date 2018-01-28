f8121ffe55e641e645b07e748428a188fcef077e
Dennis Burger <dennis@dutchwebworks.nl>
2018-01-28 12:26:21 +0100

    Multiple demo's of custom directives.

--- src/components/CustomDirectives/CustomDirective.vue
+++ src/components/CustomDirectives/CustomDirective.vue
@@ -4,7 +4,9 @@
 
         <p class="customdirective__body">Using Vue custom directive attributes to create custom functionality.</p>
 
-        <p class="customdirective__body" v-highlight:background.delayed="{ color: 'white', backgroundColor: 'orange', delay: 6000 }">Using inline attribute to set the background color of this custom directive</p>
+        <p class="customdirective__body" v-highlight="{ color: 'red' }">Simple example with one object value.</p>
+        <p class="customdirective__body" v-highlight:background="{ color: 'deeppink', backgroundColor: 'deepskyblue' }">Example directive with a directive argument 'background' and multiple object values.</p>
+        <p class="customdirective__body" v-highlight:background.delayed="{ color: 'white', backgroundColor: 'deeppink', delay: 2000, classname: 'has-hightlight' }">Example with a directive argument and a directive modifier. And also adding a classname to the element.</p>
 
         <p class="customdirective__body">Directives can be set globally or localy.</p>
     </main>
@@ -14,21 +16,19 @@
     export default {
         directives: {
             "highlight": {
-                bind(el, binding) {
-                    var delay = 0;
-                    
-                    if(binding.modifiers['delayed']) {
-                        delay = binding.value.delay;
-                    }
-
+                bind(el, binding) {                    
                     setTimeout(() => {
+                        if(binding.value.classname != undefined) {
+                            el.classList.add(binding.value.classname);
+                        }
+
                         if (binding.arg == 'background') {
                             el.style.color = binding.value.color;
                             el.style.backgroundColor = binding.value.backgroundColor;
                         } else {
                             el.style.color = binding.value.color;
                         }
-                    }, delay);	
+                    }, binding.modifiers['delayed'] ? binding.value.delay : 0);	
                 }
             } 
         }
@@ -50,6 +50,8 @@
 
     .customdirective__body[style] {
         padding: 10px;
+        border-radius: 10px;
+        -webkit-transition: background-color .5s ease-in-out, color .5s ease-in-out;
         transition: background-color .5s ease-in-out, color .5s ease-in-out;
     }
 </style>
