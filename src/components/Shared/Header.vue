a5f08b549a5a612ae9ce45075da6a87aad153381
Dennis Burger <dennis@dutchwebworks.nl>
2018-02-17 09:45:41 +0100

    Added new section with details from the Vuex store.

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -2,6 +2,18 @@
 	<header class="header">
 		<h1 class="header__title">Vue SPA learning ground</h1>
 
+		<div class="vuex-demo">
+			<h2 class="vuex-demo__title">Vuex state values</h2>			
+
+			<p class="vuex-demo__body">
+				Your cards total counter is: {{ cardsTotal | euroCurrency }}
+			</p>
+
+			<p class="vuex-demo__body">
+				Your increment is: {{ counter }}
+			</p>
+		</div>
+
 		<nav class="header__nav">
 			<ul class="header__nav-items" role="navigation">
 				<router-link tag="li" :to="{ name: 'linkHome' }" class="header__nav-item" active-class="is-active" exact-active-class="is-exact" exact><a>Home</a></router-link>
@@ -14,6 +26,19 @@
 		</nav>
 	</header>
 </template>
+
+<script>
+	export default {
+		computed: {
+			cardsTotal() {
+				return this.$store.getters.cardsTotal;
+			},
+			counter() {
+				return this.$store.getters.counter;
+			}
+		}
+	}
+</script>
 
 <style>
 	.header {
@@ -60,5 +85,32 @@
 
 	.header__nav-item.is-active a {
 		color: deepskyblue;
+	}
+
+	.vuex-demo {
+		margin-bottom: 40px;
+		padding: 20px;
+		border-radius: 8px;
+		border: 1px solid #ccc;
+	}
+
+	.vuex-demo__title,
+	.vuex-demo__body {
+		font-weight: 300;
+		font-family: "Roboto", sans-serif;
+	}
+
+	.vuex-demo__title {
+		margin-top: 0;
+		color: deepskyblue;
+		font-size: 26px;
+	}
+
+	.vuex-demo__body {
+		font-size: 18px;
+	}
+
+	.vuex-demo__body:last-child {
+		margin-bottom: 0;
 	}
 </style>
