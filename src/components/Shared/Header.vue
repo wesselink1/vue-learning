8cba18b6287fd2c0d5b98ad9ed6be64e16dacda0
Dennis Burger <dennis@dutchwebworks.nl>
2018-02-17 09:51:02 +0100

    Moved Vuex values tot separate component.

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -2,17 +2,7 @@
 	<header class="header">
 		<h1 class="header__title">Vue SPA learning ground</h1>
 
-		<div class="vuex-demo">
-			<h2 class="vuex-demo__title">Vuex state values</h2>			
-
-			<p class="vuex-demo__body">
-				Your cards total counter is: {{ cardsTotal | euroCurrency }}
-			</p>
-
-			<p class="vuex-demo__body">
-				Your increment is: {{ counter }}
-			</p>
-		</div>
+		<app-vuex-values></app-vuex-values>
 
 		<nav class="header__nav">
 			<ul class="header__nav-items" role="navigation">
@@ -28,14 +18,11 @@
 </template>
 
 <script>
+	import VuexValues from "./VuexValues.vue";
+
 	export default {
-		computed: {
-			cardsTotal() {
-				return this.$store.getters.cardsTotal;
-			},
-			counter() {
-				return this.$store.getters.counter;
-			}
+		components: {
+			"app-vuex-values": VuexValues
 		}
 	}
 </script>
@@ -85,32 +72,5 @@
 
 	.header__nav-item.is-active a {
 		color: deepskyblue;
-	}
-
-	.vuex-demo {
-		margin-bottom: 40px;
-		padding: 20px;
-		border-radius: 8px;
-		border: 1px solid #ccc;
-	}
-
-	.vuex-demo__title,
-	.vuex-demo__body {
-		font-weight: 300;
-		font-family: "Roboto", sans-serif;
-	}
-
-	.vuex-demo__title {
-		margin-top: 0;
-		color: deepskyblue;
-		font-size: 26px;
-	}
-
-	.vuex-demo__body {
-		font-size: 18px;
-	}
-
-	.vuex-demo__body:last-child {
-		margin-bottom: 0;
 	}
 </style>
