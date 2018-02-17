d221201d33163e675833f41c8b2b36e9ef89bb11
Dennis Burger <dennis@dutchwebworks.nl>
2018-02-17 13:23:31 +0100

    Moved counter related Vuex store to separate file.

--- src/store/store.js
+++ src/store/store.js
@@ -1,30 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import counter from "./modules/counter";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0,
         cardsTotal: 0
     },
     getters: {
-        counter: state => {
-            return state.counter;
-        },
         cardsTotal: state => {
             return state.cardsTotal;
         }
     },
     mutations: {
-        incrementCounter: state => {
-            state.counter++;
-        },
-        decrementCounter: state => {
-            if (state.counter > 0) {
-                state.counter--;
-            }
-        },
         incrementCardsTotal: (state, value) => {
             state.cardsTotal += value;
         },
@@ -32,7 +21,7 @@ export const store = new Vuex.Store({
             state.cardsTotal = 0;
         }
     },
-    actions: {
-        // for async (like ajax calls) actions that eventually also 'commit' a mutation
+    modules: {
+        counter
     }
 });
