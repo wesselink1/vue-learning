40260c85bb438f491899889ecca021b752c8fb1e
Dennis Burger <dennis@dutchwebworks.nl>
2018-02-18 14:28:05 +0100

    Update pug demo page.

--- src/components/Pug/Pug.vue
+++ src/components/Pug/Pug.vue
@@ -2,13 +2,19 @@
 	main.pug
 		h1.pug__title Pug template
 		p.pug__body
-			| The origin of this template is a Pug file.
-			| Webpack compiles the Pug syntax into HTML before it's send to the browser.
-			| No precompiling needed.
+			| The origin of this template is a Pug file. <strong>Webpack compiles the Pug syntax into HTML</strong> before it's send to the browser. No precompiling needed.
+
+		h2.pug__sub-title Techniques used
+		ul.pug__body
+			li Vue
+			li Vuex
+			li Vue Router
+			li JavaScript spreat operator for #[code mapGetters] &amp; #[code mapMutations]</code>
 </template>
 
 <style>
 	.pug__title,
+	.pug__sub-title,
 	.pug__body {
 		font-family: "Roboto", sans-serif;
 	}
@@ -17,5 +23,9 @@
 		font-weight: 300;
 		font-size: 32px;
 		color: deepskyblue;
+	}
+
+	.pug__sub-title {
+		font-size: 42px;
 	}
 </style>
