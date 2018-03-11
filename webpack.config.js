d2ebb315498faa16342787146425f5a21897afb7
Dennis Burger <dennis@dutchwebworks.nl>
2018-03-11 10:52:16 +0100

    Building version of Github pages.

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: "/dist/",
+    path: path.resolve(__dirname, './docs'),
     filename: 'build.js'
   },
   module: {
