4469a5abc1ec8f1d1c2bd08b50d813c2f34198ed
Dennis Burger <dennis@dutchwebworks.nl>
2018-04-27 21:16:08 +0200

    Updated footer with link to Github code.

--- src/components/Shared/Footer.vue
+++ src/components/Shared/Footer.vue
@@ -1,20 +1,37 @@
 <template>
 	<footer class="footer">
 		<p class="footer__body">By Dennis Burger, march 2018</p>
+		<p class="footer__body"><a href="https://github.com/dutchwebworks/vue-learning" target="_blank">View on Github</a></p>
 	</footer>
 </template>
 
 <style lang="scss">
 	@import "../../scss/style";
 
+	.footer {
+		@include display-grid;
+		@include grid-template-columns(1fr 1fr);
+	}
+
+	.footer__body,
+	.footer__body a {
+		color: map-get($colors, 03);
+		font-size: 14px;
+		font-weight: 300;
+		font-family: $font-custom;
+	}
+
 	.footer__body {
 		margin-top: 30px;
 		padding-top: 10px;
 		border-top: 1px solid map-get($colors, 04);
-		color: map-get($colors, 04);
+	}
+
+	.footer__body + .footer__body {
 		text-align: right;
-		font-size: 14px;
-		font-weight: 300;
-		font-family: $font-custom;
+	}
+
+	.footer__body a:hover {
+		color: map-get($colors, 02);
 	}
 </style>
