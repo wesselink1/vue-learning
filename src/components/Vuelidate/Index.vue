cdc7fe656017eed269f3ec53ff733d0926eb9c9b
Dennis Burger <dennis@dutchwebworks.nl>
2018-04-27 20:32:38 +0200

    Vuelidate on coupon codes.

--- src/components/Vuelidate/Index.vue
+++ src/components/Vuelidate/Index.vue
@@ -1,128 +1,172 @@
 <template>
 	<section class="vuelidate">
-		<h1 class="heading">Form validation using Vuelidate</h1>
+		<div class="vuelidate-grid">
+			<div class="vuelidate-grid__form">
+				<h2 class="heading">Form validation using Vuelidate</h2>
 
-		<p class="paragraph">Using 3rd party form validation plugin to validate form fields. When they are invalid they show a CSS animation (shake).</p>
+				<p class="paragraph">Using 3rd party form validation plugin to validate form fields. When they are invalid they show a CSS animation (shake).</p>
 
-		<form>
-			<div class="vuelidate-grid">
-				<label 
-					class="vuelidate__label vuelidate__label--input-field"
-					:class="{ 'has-error' : $v.username.$error, 'is-valid' : (!$v.username.$error && $v.username.$dirty) }">
-					Name
-				</label>
+				<form>
+					<div class="vuelidate-form-grid">
+						<label 
+							class="vuelidate__label vuelidate__label--input-field"
+							:class="{ 'has-error' : $v.username.$error, 'is-valid' : (!$v.username.$error && $v.username.$dirty) }">
+							Name
+						</label>
 
-				<div
-					class="vuelidate__values"
-					:class="{ 'do-shake' : $v.username.$error }">
-					<input
-						type="text"
-						name="username"
-						class="vuelidate__input"
-						:class="{ 'has-error' : $v.username.$error, 'is-valid' : (!$v.username.$error && $v.username.$dirty) }"
-						@blur="$v.username.$touch()"
-						v-model="username">
+						<div
+							class="vuelidate__values"
+							:class="{ 'do-shake' : $v.username.$error }">
+							<input
+								type="text"
+								name="username"
+								class="vuelidate__input"
+								:class="{ 'has-error' : $v.username.$error, 'is-valid' : (!$v.username.$error && $v.username.$dirty) }"
+								@blur="$v.username.$touch()"
+								v-model="username">
+								
+							<small
+								class="vuelidate__error"
+								v-if="$v.username.$error">
+								Please enter you name
+							</small>
+						</div>
+
+						<label 
+							class="vuelidate__label vuelidate__label--input-field"
+							:class="{ 'has-error' : $v.email.$error, 'is-valid' : (!$v.email.$error && $v.email.$dirty) }">
+							E-mail
+						</label>
+
+						<div
+							class="vuelidate__values"
+							:class="{ 'do-shake' : $v.email.$error }">
+							<input
+								type="email"
+								name="email"
+								class="vuelidate__input"
+								:class="{ 'has-error' : $v.email.$error, 'is-valid' : (!$v.email.$error && $v.email.$dirty) }"
+								@blur="$v.email.$touch()"
+								v-model="email">
+
+							<small
+								class="vuelidate__error"
+								v-if="$v.email.$error">
+								Please enter an email address
+							</small>
+
+							<small 
+								class="vuelidate__error"
+								v-if="!$v.email.email">
+								Please provide a valid email address
+							</small>
+						</div>
+
+						<label
+							class="vuelidate__label vuelidate__label--input-field"
+							:class="{ 'has-error' : $v.age.$error, 'is-valid' : (!$v.age.$error && $v.age.$dirty) }">
+							Age
+						</label>
+
+						<div
+							class="vuelidate__values"
+							:class="{ 'do-shake' : $v.age.$error }">
+							<input 
+								type="number" 
+								class="vuelidate__input"
+								:class="{ 'has-error' : $v.age.$error, 'is-valid' : (!$v.age.$error && $v.age.$dirty) }"
+								v-model.number="age"
+								:placeholder="'Minimum age is ' +  $v.age.$params.minVal.min"
+								@blur="$v.age.$touch()">
+
+							<small
+								class="vuelidate__error"
+								v-if="$v.age.$error">
+								Minimum age is {{ $v.age.$params.minVal.min }}
+							</small>
+						</div>
+
+						<label
+							class="vuelidate__label vuelidate__label--input-field">
+							Available coupons
+						</label>
+
+						<div class="vuelidate__values">
+							<ul class="vuelidate__coupons">
+								<li v-for="coupon in coupons">{{ coupon }}</li>
+							</ul>
+						</div>
+
+						<label
+							class="vuelidate__label vuelidate__label--input-field"
+							:class="{ 'has-error' : $v.coupon.$error, 'is-valid' : $v.coupon.validCoupon && $v.coupon.$dirty }">
+						 	Coupon
+						</label>
 						
-					<small
-						class="vuelidate__error"
-						v-if="$v.username.$error">
-						Please enter you name
-					</small>
-				</div>
+						<div
+							class="vuelidate__values"
+							:class="{ 'do-shake' : $v.coupon.$error, 'is-valid' : $v.coupon.validCoupon && $v.coupon.$dirty }">
 
-				<label 
-					class="vuelidate__label vuelidate__label--input-field"
-					:class="{ 'has-error' : $v.email.$error, 'is-valid' : (!$v.email.$error && $v.email.$dirty) }">
-					E-mail
-				</label>
+							<label>
+								<input 
+									type="text"
+									class="vuelidate__input"
+									:class="{ 'has-error' : $v.coupon.$error, 'is-valid' : $v.coupon.validCoupon && $v.coupon.$dirty }"
+									v-model="coupon"
+									placeholder="Try out one of the above coupon codes"
+									@blur="$v.coupon.$touch()">
 
-				<div
-					class="vuelidate__values"
-					:class="{ 'do-shake' : $v.email.$error }">
-					<input
-						type="email"
-						name="email"
-						class="vuelidate__input"
-						:class="{ 'has-error' : $v.email.$error, 'is-valid' : (!$v.email.$error && $v.email.$dirty) }"
-						@blur="$v.email.$touch()"
-						v-model="email">
+								<small 
+									class="vuelidate__error"
+									v-if="$v.coupon.$error && !$v.coupon.isEmpty">
+									Coupon code is invalid
+								</small>
+							</label>
+						</div>					
 
-					<small
-						class="vuelidate__error"
-						v-if="$v.email.$error">
-						Please enter an email address
-					</small>
+						<label
+							class="vuelidate__label"
+							:class="{ 'is-valid' : terms }">
+						 	Terms
+						</label>
 
-					<small 
-						class="vuelidate__error"
-						v-if="!$v.email.email">
-						Please provide a valid email address
-					</small>
-				</div>
+						<div
+							class="vuelidate__values"
+							:class="{ 'do-shake' : $v.terms.$error }">
+							<label 
+								:class="{ 'is-valid' : terms }">
+								<input 
+									type="checkbox"
+									v-model="terms"
+									@change="$v.terms.$touch()">
+									I accept the terms of usage
+							</label>
 
-				<label
-					class="vuelidate__label vuelidate__label--input-field"
-					:class="{ 'has-error' : $v.age.$error, 'is-valid' : (!$v.age.$error && $v.age.$dirty) }">
-					Age
-				</label>
+							<small
+								class="vuelidate__error"
+								v-if="$v.terms.$error">
+								You must accept our terms and conditions to proceed
+							</small>
+						</div>
 
-				<div
-					class="vuelidate__values"
-					:class="{ 'do-shake' : $v.age.$error }">
-					<input 
-						type="number" 
-						class="vuelidate__input"
-						:class="{ 'has-error' : $v.age.$error, 'is-valid' : (!$v.age.$error && $v.age.$dirty) }"
-						v-model.number="age"
-						:placeholder="'Minimum age is ' +  $v.age.$params.minVal.min"
-						@blur="$v.age.$touch()">
+						<button
+							class="vuelidate__button button button--02"
+							:disabled="$v.$invalid"
+							@click.prevent="submitForm">
+							Send form
+						</button>
+					</div>
+				</form>
+			</div>
 
-					<small
-						class="vuelidate__error"
-						v-if="$v.age.$error">
-						Minimum age is {{ $v.age.$params.minVal.min }}
-					</small>
-				</div>
-
-				<label
-					class="vuelidate__label"
-					:class="{ 'is-valid' : terms }">
-				 	Terms
-				</label>
-
-				<div
-					class="vuelidate__values"
-					:class="{ 'do-shake' : $v.terms.$error }">
-					<label 
-						:class="{ 'is-valid' : terms }">
-						<input 
-							type="checkbox"
-							v-model="terms"
-							@change="$v.terms.$touch()">
-							I accept the terms of usage
-					</label>
-
-					<small
-						class="vuelidate__error"
-						v-if="$v.terms.$error">
-						You must accept our terms and conditions to proceed
-					</small>
-				</div>
-
-				<button
-					class="vuelidate__button button button--02"
-					:disabled="$v.$invalid"
-					@click.prevent="submitForm">
-					Send form
-				</button>
-
-				<div class="vuelidate__label">Vuelidate validation object</div>
+			<div class="vuelidate-grid__validation">
+				<h2 class="heading">Vuelidate validation object</h2>
 
 				<div class="vuelidate__values">
 					<pre>{{ $v }}</pre>
 				</div>
 			</div>
-		</form>
+		</div>
 	</section>
 </template>
 
@@ -135,23 +179,38 @@
 				age: null,
 				email: null,
 				terms: false,
-				username: null
+				username: null,
+				coupon: null,
+				coupons: [
+					"SUMMER28",
+					"FALL44",
+					"SPRING2019"
+				]
 			}
 		},
 		validations: {
-			age: {
+			username: {
 				required,
-				minVal: minValue(18)
 			},
 			email: {
 				required,
 				email
 			},
+			age: {
+				required,
+				minVal: minValue(18)
+			},
+			coupon: {
+				validCoupon(val) {
+					if(val == "" || val == null) {
+						return true; 
+					} else {
+						return this.coupons.includes(val);
+					}
+				}
+			},
 			terms: {
 				required
-			},
-			username: {
-				required,
 			}
 		},
 		methods: {
@@ -173,6 +232,12 @@
 	}
 
 	.vuelidate-grid {
+		@include display-grid;
+		@include grid-template-columns(2fr 1fr);
+		grid-gap: 40px;
+	}
+
+	.vuelidate-form-grid {
 		@include display-grid;
 		@include grid-template-columns(200px 1fr);
 		grid-gap: 20px;
@@ -209,7 +274,15 @@
 		&.has-error {
 			border-color: red;
 		}
-	}		
+	}
+
+	.vuelidate__coupons {
+		margin: 0 0 0 20px;
+		padding: 0;
+		font-family: monospace, sans-serif;
+		color: map-get($colors, 2);
+		font-size: 22px;
+	}	
 
 	.vuelidate__error {
 		display: block;
