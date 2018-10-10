e36e31a8b746540d2220a4566eb08fd0946d5cd6
Dennis Burger <dennis.burger@poort80.nl>
2018-10-10 10:06:17 +0200

    Added required Vue v-for keys. Updated copy on page for documentation.

--- src/views/TvShowsPage.vue
+++ src/views/TvShowsPage.vue
@@ -6,9 +6,19 @@
 
         <ul>
             <li
-                v-for="show in tvshows">
+                v-for="show in tvshows"
+                :key="show['.key']">
                 {{ show.title }} ({{show.year}})
             </li>
+        </ul>
+
+        <h2 class="sub-heading">Prerequites</h2>
+
+        <p class="paragraph">These are the extra NPM package you'll need to get easy access to Google Firebase (database).</p>
+
+        <ul>
+            <li><a href="https://www.npmjs.com/package/firebase" target="_blank">firebase</a></li>
+            <li><a href="https://www.npmjs.com/package/vuefire" target="_blank">Vuefire</a></li>
         </ul>
     </div>
 </template>
