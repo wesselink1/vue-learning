8a869319b9f7667ec06a4d90f9269c97f282dcb7
Dennis Burger <dennis@dutchwebworks.nl>
2018-12-22 16:46:00 +0100

    Renamed Firebase collection

--- src/db/index.js
+++ src/db/index.js
@@ -17,7 +17,7 @@ const FireBaseApp = Firebase.initializeApp(config);
 const FireDB = FireBaseApp.database();
 const firebase = function() {
     return {
-        tvshows: FireDB.ref("tvshowsWatched"),
+        tvshows: FireDB.ref("tvshows"),
         cards: FireDB.ref("cards")
     }
 }
